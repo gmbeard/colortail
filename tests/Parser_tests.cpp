84974cc34fb8499d6db092a8fd5c125bf2e94acc
Greg Beard <greg.beard@vodafone.com>
2015-11-08 20:44:11 +0000

    Parser in tests now correctly parses config block

--- tests/Parser_tests.cpp
+++ tests/Parser_tests.cpp
@@ -28,117 +28,203 @@
 namespace
 {
   /*
-    config_block  ::= '{', (config_line)*, '}'
+    config_block  ::= '{', newline, (config_line)*, '}'
     config_line   ::= (any_non_newline)*, newline
   */
   char const *kParseLine1 =
-    "{\nOne\nTwo\nThree\n}";
+    "{\nOne\nTwo\nThr}ee\n}";
 
-  template<typename T>
-  struct exact_value
+  enum ids {
+    CONFIG_BLOCK_START,
+    CONFIG_BLOCK_END,
+    CONFIG_LINE_START,
+    CONFIG_LINE_END,
+    CONFIG_ITEM_DATA,
+    IGNORE
+  };
+
+  template<typename Fn>
+  struct prod_base
   {
-    exact_value(T t)
-      : t_(t)
+    typedef Fn callback_type;
+
+    prod_base(callback_type f)
+      : f_(f)
     { }
 
-    template<typename U>
-    friend bool matches(U c, exact_value<U> const *prod)
+    template<typename T>
+    void operator()(T t, ids id) const
+    {
+      const_cast<Fn &>(f_)(t, id);
+    }
+
+    callback_type const & callback() const
+    {
+      return f_;
+    }
+  private:
+    callback_type f_;
+  };
+
+  template<typename T, typename Callback>
+  struct exact_value : prod_base<Callback>
+  {
+
+    exact_value(T t, ids id, Callback f )
+      : prod_base<Callback>(f),
+        t_(t),
+        id_(id)
+    { }
+
+    template<typename U, typename Fn>
+    friend bool matches(U c, exact_value<U, Fn> const *prod)
     {
       return (c == (*prod).t_);
     }
 
-    template<typename U>
-    friend bool evaluate(U c, exact_value<U> const *prod)
+    template<typename U, typename Fn>
+    friend bool evaluate(U c, exact_value<U, Fn> const *prod)
     {
       return matches(c, prod);
     }
 
-    template<typename U>
+    template<typename U, typename Fn>
     friend void expand(U c, 
       colortail::parser::expression_stack &s, 
-        exact_value<U> const *prod)
+        exact_value<U, Fn> const *prod)
     {
       push(s, *prod);
+      (*prod)(c, prod->id_);
     }
 
   private:
     T t_;
+    ids id_;
   };
 
-  struct new_line_prod
+  template<typename Fn>
+  struct new_line_prod : prod_base<Fn>
   {
+    new_line_prod(ids id, Fn f)
+      : prod_base<Fn>(f),
+        id_(id)
+    { }
+
     friend bool evaluate(char c, new_line_prod const *)
     {
       return (c == '\r' || c == '\n');
     }
 
     friend void expand(char c, colortail::parser::expression_stack &s,
-      new_line_prod const *)
+      new_line_prod const *prod)
     {
       if(c == '\r') {
-        push(s, new_line_prod());
+        push(s, new_line_prod<Fn>(prod->id_, prod->callback()));
       }
-      push(s, exact_value<char>(c));
+      else {
+        (*prod)(c, prod->id_);
+      }
+      push(s, exact_value<char, Fn>(c, prod->id_, prod->callback()));
     }
+  private:
+    ids id_;
   };
 
-  struct any_non_newline
+  template<typename Fn>
+  struct any_non_newline : prod_base<Fn>
   {
-    friend bool evaluate(char c, any_non_newline const *)
+    any_non_newline(ids id, Fn f, gmb::memory::shared_ptr<size_t> n)
+      : prod_base<Fn>(f),
+        id_(id),
+        n_(n)
+    { }
+
+    friend bool evaluate(char c, any_non_newline const *prod)
     {
+      size_t n = *prod->n_;
+      if(n == 0 && c == '}') {
+        return false;
+      }
+
       return c != '\n' && c != '\r';
     }
 
     friend void expand(char c, colortail::parser::expression_stack &s,
-      any_non_newline const *)
+      any_non_newline const *prod)
     {
-      push(s, exact_value<char>(c));
+      push(s, exact_value<char, Fn>(c, prod->id_, prod->callback()));
+      (*prod)(c, prod->id_); 
+      (*prod->n_)++;
     }
+
+  private:
+    ids     id_;
+    mutable gmb::memory::shared_ptr<size_t> n_;
   };
 
-  struct config_line
+  template<typename Fn>
+  struct config_line : prod_base<Fn>
   {
-    config_line()
-      : repeat_(any_non_newline(), new_line_prod())
+    config_line(Fn f)
+      : prod_base<Fn>(f),
+        n_(new size_t(0)),
+        repeat_(any_non_newline<Fn>(CONFIG_ITEM_DATA, f, n_), 
+          new_line_prod<Fn>(CONFIG_LINE_END, f))
     { }
 
     friend bool evaluate(char c, config_line const *prod)
     {
-      return evaluate(c, &(*prod).repeat_);
+       *prod->n_ = 0;
+       if(evaluate(c, &(prod->repeat_))) {
+         return true;
+       }
+       
+       return false;
     }
 
     friend void expand(char c, colortail::parser::expression_stack &s,
       config_line const *prod)
     {
-      expand(c, s, &(*prod).repeat_);
+      expand(c, s, &(prod->repeat_));
+      (*prod)(c, CONFIG_LINE_START);
     }
   private:
+    mutable gmb::memory::shared_ptr<size_t> n_;
     colortail::parser::repeat_until_expression repeat_;
   };
 
-  struct config_block
+  template<typename Fn>
+  struct config_block : prod_base<Fn>
   {
+    config_block(Fn f)
+      : prod_base<Fn>(f)
+    { }
+
     friend bool evaluate(char c, config_block const *)
     {
       return c == '{';
     }
 
     friend void expand(char c, colortail::parser::expression_stack &s,
-      config_block const *)
+      config_block const *prod)
     {
       typedef colortail::parser::repeat_until_expression
         repeater;
 
-      push(s, repeater(config_line(), exact_value<char>('}')));
-      push(s, exact_value<char>('{'));;
+      push(s, repeater(config_line<Fn>(prod->callback()), 
+        exact_value<char, Fn>('}', CONFIG_BLOCK_END, prod->callback())));
+      push(s, new_line_prod<Fn>(IGNORE, prod->callback()));
+      push(s, exact_value<char, Fn>('{', IGNORE, prod->callback()));;
+      (*prod)(c, CONFIG_BLOCK_START);
     }
+
   };
 
-  template<typename InputIterator>
-  bool parse_input(InputIterator first, InputIterator last)
+  template<typename InputIterator, typename Callback>
+  bool parse_input(InputIterator first, InputIterator last, Callback cb)
   {
     colortail::parser::expression_stack stack;
-    push(stack, config_block());
+    push(stack, config_block<Callback>(cb));
     InputIterator curr = first;
     colortail::parser::unique_expression_ptr expr;
 
@@ -159,6 +245,35 @@ namespace
 
     return true;
   }
+
+  struct data_callback
+  {
+    data_callback(ids id, size_t *p)
+      : id_(id),
+        p_(p)
+    { }
+
+    data_callback(ids id)
+      : id_(id),
+        p_(NULL)
+    { }
+
+    void operator()(char c, ids i)
+    {
+      if(p_ && i == id_) {
+        (*p_)++;
+      }
+    }
+
+    size_t const & count() const
+    {
+      return *p_;
+    }
+
+  private:
+    ids id_;
+    size_t *p_;
+  };
 }
 
 TEST_CASE("Parser Tests")
@@ -179,16 +294,20 @@ TEST_CASE("Parser Tests")
   SECTION("Pushing a literal value on the stack results in a match for the top item")
   {
     namespace ctp = colortail::parser;
-
+    data_callback cb(IGNORE);
     ctp::expression_stack stack;
-    push(stack, exact_value<char>('g'));
+    push(stack, exact_value<char, data_callback>('g', IGNORE, cb));
   
     REQUIRE(top(stack).matches('g'));
   }
 
   SECTION("Parsing test config block succeeds")
   {
-    REQUIRE(parse_input(kParseLine1, kParseLine1 + strlen(kParseLine1)));
+    size_t n = 0;
+    data_callback cb(CONFIG_LINE_START, &n);
+
+    REQUIRE(parse_input(kParseLine1, kParseLine1 + strlen(kParseLine1), cb));
+    REQUIRE(cb.count() == 3);
   }
 }
 
